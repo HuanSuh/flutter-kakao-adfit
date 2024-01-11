76fc28463d9edbdccebe5eeb2586d460c22af87e
huansuh <huansuh@jobis.co>
2024-01-11 11:45:19 +0900

    fix: view build issue 수정

--- android/src/main/kotlin/flutter/kakao/flutter_adfit/AdViewFactory.kt
+++ android/src/main/kotlin/flutter/kakao/flutter_adfit/AdViewFactory.kt
@@ -5,51 +5,35 @@ import android.content.Context
 import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.JSONMessageCodec
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import io.flutter.plugin.platform.PlatformViewFactory
 
 class AdViewFactory
-    private constructor(private val messenger: BinaryMessenger, private val appContext: Context)
+    private constructor(private val messenger: BinaryMessenger)
     : PlatformViewFactory(JSONMessageCodec.INSTANCE) {
 
-    var activity: Activity? = null
-    private var adView: NativeAdView? = null
+    lateinit var activity: Activity
+    private lateinit var adView: NativeAdView
 
-    override fun create(context: Context, id: Int, args: Any): NativeAdView? {
-        activity?.let {
-            adView = NativeAdView(it, messenger, id, args)
-        }
+    override fun create(context: Context?, id: Int, args: Any?): NativeAdView {
+        adView = NativeAdView(activity, messenger, id, args)
         return adView
     }
 
     fun onDestroy() {
-        adView?.dispose()
+        adView.dispose()
     }
 
     companion object {
         /**
-         * Flutter Android v1 API (using Registrar)
-         */
-        fun registerWith(registrar: Registrar): AdViewFactory {
-            val plugin = AdViewFactory(registrar.messenger(), registrar.activity())
-            registrar.platformViewRegistry().registerViewFactory("flutter.kakao.adfit/AdFitView", plugin)
-            registrar.addViewDestroyListener {
-                plugin.onDestroy()
-                false
-            }
-            return plugin
-        }
-
-        /**
          * Flutter Android v2 API (using FlutterPluginBinding)
          */
         fun registerWith(flutterPluginBinding: FlutterPluginBinding): AdViewFactory {
-            val plugin = AdViewFactory(flutterPluginBinding.binaryMessenger,
-                    flutterPluginBinding.applicationContext)
+            val plugin = AdViewFactory(flutterPluginBinding.binaryMessenger)
             flutterPluginBinding.platformViewRegistry.registerViewFactory(
                     "flutter.kakao.adfit/AdFitView", plugin)
             return plugin
         }
     }
+
 
 }
